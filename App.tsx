041ea780dc82275ce40b7419712bf18db888247d
Daniel Sukenik <sukenik9@gmail.com>
2023-03-11 21:15:32 +0200

    Finished client

--- App.tsx
+++ App.tsx
@@ -1,14 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!🚀</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Image, StyleSheet, View } from 'react-native'
+import Winners from './src/Components/Winners'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { useCallback } from 'react'
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +10,38 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    direction: 'rtl'
   },
-});
+  image: {
+    height: '60%',
+    width: '90%',
+    marginTop: 30,
+    marginBottom: 10
+  }
+})
+
+SplashScreen.preventAutoHideAsync()
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Alef-Regular': require('./assets/fonts/Alef-Regular.ttf'),
+    'Alef-Bold': require('./assets/fonts/Alef-Bold.ttf')
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+  
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Image source={require('./assets/toran-zevel-2.0.jpeg')} style={styles.image} />
+      <Winners />
+    </View>
+  )
+}
